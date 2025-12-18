470c2700ad65759f6887a6b1125365be961d4520
risarisarisa <from.aqua.to@gmail.com>
2025-12-18 18:43:13 +1100

    Added props to Headline

--- app/components/Headline.tsx
+++ app/components/Headline.tsx
@@ -1,24 +1,34 @@
 
-export default function Headline() {
+
+type HeadlineProps = {
+  title: string;
+  page: string;
+};
+
+export default function Headline(props: HeadlineProps) {
+
   return (
     <>
-     <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Hey! Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+      <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+        To get started, edit the page.tsx file. This is {props.title}
+      </h1>
+      <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+        Hey! This is {props.page} page. Looking for a starting point or more instructions? Head over to{" "}
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="font-medium text-zinc-950 dark:text-zinc-50"
+        >
+          Templates
+        </a>{" "}
+        or the{" "}
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="font-medium text-zinc-950 dark:text-zinc-50"
+        >
+          Learning
+        </a>{" "}
+        center.
+      </p>
     </>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -14,11 +14,9 @@ export default function Home() {
           priority
         />
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
+
           
-          <Headline />
+          <Headline title="Home page" page="Home" />
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
