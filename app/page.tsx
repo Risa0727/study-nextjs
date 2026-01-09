453822efd99a6a31fac64a42700d801994df0b41
risarisarisa <from.aqua.to@gmail.com>
2026-01-09 17:47:26 +1100

    Change to use css modules

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Headline from "./components/Headline";
+import styles from "./styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className={styles.wrapper}>
+      <main className={styles.main}>
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -13,20 +14,17 @@ export default function Home() {
           height={20}
           priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-
-          
+        <div className={styles.content}>
           <Headline title="Home page" page="Home" />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
+        <div className={styles.buttons}>
           <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className={styles.buttonPrimary}
+            href="https://vercel.com/new"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
-              className="dark:invert"
               src="/vercel.svg"
               alt="Vercel logomark"
               width={16}
@@ -35,8 +33,8 @@ export default function Home() {
             Deploy Now
           </a>
           <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className={styles.buttonSecondary}
+            href="https://nextjs.org/docs"
             target="_blank"
             rel="noopener noreferrer"
           >
